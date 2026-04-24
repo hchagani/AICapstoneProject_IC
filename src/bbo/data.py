f4e6e3da14416e38ddf0895a4bdb1bf501060476
Hassan Chagani <chagani.hassan@gmail.com>
2026-04-24 13:35:32 +0100

    Add maximum from final data set to final result

--- src/bbo/data.py
+++ src/bbo/data.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 # List of number of initial data points in order of function ID
 N_POINTS = [10, 10, 15, 30, 20, 20, 30, 40]
+FINAL_WEEK = 14  # After all queries have been processed
 
 
 def get_data_dir() -> Path:
@@ -55,3 +56,32 @@ def get_current_weeks_points(
     n_points = N_POINTS[function_id - 1] + week - 1
 
     return load_data(function_id, n_points)
+
+
+def get_final_maximum(function_id: int) -> tuple[np.ndarray, float, float]:
+    """Get coordinates and output for data point that yields maximum output
+    after all queries have been submitted (i.e. for the final result). Compare
+    this point with the maximum output from the initial data set.
+
+    Args:
+        function_id (int): function ID from 1 to 8.
+
+    Returns:
+        tuple of coordinates of best point, output from best point, and
+          difference between outputs from best point and that from initial data
+          set.
+    """
+    # Get indices of best points from final and initial data sets
+    X, y = get_current_weeks_points(function_id, week=FINAL_WEEK)
+    final_max_idx = np.argmax(y)
+    initial_max_idx = np.argmax(y[:N_POINTS[function_id - 1]])
+
+    final_max_input = X[final_max_idx]
+    final_max_output = y[final_max_idx]
+    initial_max_output = y[initial_max_idx]
+
+    return (
+        final_max_input,
+        final_max_output,
+        final_max_output - initial_max_output,
+    )
